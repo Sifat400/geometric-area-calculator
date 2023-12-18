11d19b068a60b77b9e47d153f499013c1d6c3f7d
Sifat <abdullahsifat400@gmail.com>
2023-12-18 22:45:34 +0600

    added input validation and fixing some issus

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,11 @@ document
   .addEventListener("click", function () {
     const base = getInputValue("triangle-base");
     const height = getInputValue("triangle-height");
+    // Input Validation
+    if (isNaN(base) || isNaN(height)) {
+      alert("Please insert Number");
+      return;
+    }
     const area = 0.5 * base * height;
 
     setElementInnerText("area-triangle", area);
@@ -15,6 +20,11 @@ document
   .addEventListener("click", function () {
     const width = getInputValue("rectangle-width");
     const length = getInputValue("rectangle-length");
+    // Input Validation
+    if (isNaN(width) || isNaN(length)) {
+      alert("Please insert Number");
+      return;
+    }
     const area = width * length;
     setElementInnerText("area-rectangle", area);
   });
@@ -25,6 +35,11 @@ document
   .addEventListener("click", function () {
     const width = getInputValue("parallelogram-width");
     const length = getInputValue("parallelogram-length");
+    // Input Validation
+    if (isNaN(width) || isNaN(length)) {
+      alert("Please insert Number");
+      return;
+    }
     const area = width * length;
     setElementInnerText("area-parallelogram", area);
   });
@@ -35,6 +50,11 @@ document
   .addEventListener("click", function () {
     const diagonal1 = getInputValue("rhombus-diagonal-1");
     const diagonal2 = getInputValue("rhombus-diagonal-2");
+    // Input Validation
+    if (isNaN(diagonal1) || isNaN(diagonal2)) {
+      alert("Please insert Number");
+      return;
+    }
     const area = 0.5 * diagonal1 * diagonal2;
     setElementInnerText("area-rhombus", area);
   });
@@ -45,6 +65,11 @@ document
   .addEventListener("click", function () {
     const perimeter = getInputValue("perimeter");
     const apothem = getInputValue("apothem");
+    // Input Validation
+    if (isNaN(perimeter) || isNaN(apothem)) {
+      alert("Please insert Number");
+      return;
+    }
     const area = 0.5 * perimeter * apothem;
     setElementInnerText("area-pentagon", area);
   });
@@ -57,6 +82,12 @@ document
     const a = getInputValue("ellipse-a");
     const b = getInputValue("ellipse-b");
     const pi = Math.PI;
+    // Input Validation
+    if (isNaN(a) || isNaN(b)) {
+      alert("Please insert Number");
+      return;
+    }
     const area = pi * a * b;
-    setElementInnerText("area-ellipse", area);
+    const areaTwoDecimal = area.toFixed(2);
+    setElementInnerText("area-ellipse", areaTwoDecimal);
   });
